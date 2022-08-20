056c4e5d5abb61059c0af0d224055718e464d40b
AnaSantana <anapedra.mil@gmail.com>
2022-08-20 10:34:59 -0300

    primeiro comite

---
+++ src/main/java/com/junirpedroanasatana/virtualstror/VirtualStrorApplication.java
@@ -0,0 +1,13 @@
+package com.junirpedroanasatana.virtualstror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualStrorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junirpedroanasatana/virtualstror/VirtualStrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junirpedroanasatana.virtualstror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
